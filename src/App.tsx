c8764d54d70daa369429c4e374bdca5e741b3d1a
Jamy <63951751+Park-Jamy@users.noreply.github.com>
2025-01-28 16:19:36 +0900

    20250128 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 
 const App: React.FC = () => {
+
+  var foo = 123;
+  console.log(foo)
+
   return (
     <div>
       <h1>hello my first react project</h1>
       <h1>branch change</h1>
-      <h1>branch change2</h1>
-      <h1>branch change3</h1>
-      <h1>main change</h1>
     </div>
   );
 };
---
+++ src/practice/p_250128.ts
@@ -0,0 +1,34 @@
+// var / const 차이
+
+
+// var
+// 중복 선언 가능 , 위 선언 된 foo를 블럭 안에서도 참조가 가능
+var foo = 123;
+console.log(foo);
+
+{
+    var foo = 456;
+}
+console.log(foo); 
+
+
+// const
+// 상수화 시킬 때 사용
+// 중간 값을 변경하지 못함
+// 객체에도 사용이 가능 key : value로 객체 저장 가능
+
+const obj = {id: 123};
+const objp = {pw: 456};
+
+const user_ = {
+    name : 'devtaco',
+    address: {
+        city : 'Seoul'
+    }
+}
+
+console.log(user_);
+user_.name = 'PARK';
+console.log(user_);
+user_.address.city = 'Busan';
+console.log(user_);
